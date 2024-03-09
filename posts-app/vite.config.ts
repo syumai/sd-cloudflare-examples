f58a6843b394e50c4d705c862ef2c997201e1a24
syumai <syumai@gmail.com>
2024-03-09 18:44:17 +0900

    add comment about wrangler's getPlatformProxy func

--- posts-app/vite.config.ts
+++ posts-app/vite.config.ts
@@ -3,6 +3,8 @@ import devServer from '@hono/vite-dev-server'
 import { defineConfig } from 'vite'
 import { getEnv } from '@hono/vite-dev-server/cloudflare-pages'
 
+// NOTE: WranglerのgetPlatformProxy関数を使うと、より簡潔に設定ができます
+// - https://github.com/syumai/sd-cloudflare-examples/blob/use-get-platform-proxy-in-vite-config/posts-app/vite.config.ts#L7
 export default defineConfig({
   plugins: [
     build(),
